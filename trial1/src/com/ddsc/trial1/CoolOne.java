55f54d5a848f51692234d696133b72f0e948d87a
rss245 <rss245@gmail.com>
2015-12-10 01:37:24 -0500

    initial commit

---
+++ trial1/src/com/ddsc/trial1/CoolOne.java
@@ -0,0 +1,11 @@
+package com.ddsc.trial1;
+
+public class CoolOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("coolone");
+        
+	}
+
+}
