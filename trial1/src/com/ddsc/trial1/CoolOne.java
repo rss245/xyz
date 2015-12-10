1975680a3f89aa51236c6ee67e2641178955200d
rss245 <rss245@gmail.com>
2015-12-10 01:43:15 -0500

    Now

--- trial1/src/com/ddsc/trial1/CoolOne.java
+++ trial1/src/com/ddsc/trial1/CoolOne.java
@@ -4,7 +4,7 @@ public class CoolOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("coolone");
+        System.out.println("coolone now");
         
 	}
 
