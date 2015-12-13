ad82f88c7a416089b96f0a82c5664247b2faf172
rss245 <rss245@gmail.com>
2015-12-13 04:09:27 -0500

    so long and thanks for the fish

---
+++ hello/src/com/ddsc/test2/Howdy.java
@@ -0,0 +1,11 @@
+package com.ddsc.test2;
+
+public class Howdy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Howdy there");
+
+	}
+
+}
